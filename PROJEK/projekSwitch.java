7aa826911e34606217cec9676a501e3a6f49738c
MuhammadRendra <abas.bashori1612@gmail.com>
2025-11-04 21:07:37 +0700

    pembuatan declare dan pemanggilan variabel

--- PROJEK/projekSwitch.java
+++ PROJEK/projekSwitch.java
@@ -7,5 +7,18 @@ public class projekSwitch {
        Scanner qs = new Scanner(System.in);
 
        System.out.println("Nilai Matkul");
+
+       double strukdat, PBO, basdat, algopem, repelu, mean;
+
+        System.out.println("Masukkan Nilai Struktur Data: ");
+        strukdat = qs.nextDouble();
+        System.out.println("Masukkan Nilai Pemrograman Berorientasi Objek: ");
+        PBO = qs.nextDouble();
+        System.out.println("Masukkan Nilai Basis Data: ");
+        basdat = qs.nextDouble();
+        System.out.println("Masukkan Nilai Algoritma dan Pemrograman: ");
+        algopem = qs.nextDouble();
+        System.out.println("Masukkan Nilai Rekayasa Perangkat Lunak: ");
+        repelu = qs.nextDouble();
     }
 }
