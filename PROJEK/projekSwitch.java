e5c37aab8c2cb915f883b8735db5f7cc90fac2c9
MuhammadRendra <abas.bashori1612@gmail.com>
2025-11-04 21:03:02 +0700

    nambahin scanner dan membuat sysout nilai matkul

--- PROJEK/projekSwitch.java
+++ PROJEK/projekSwitch.java
@@ -1,7 +1,11 @@
 package PROJEK;
 
+import java.util.Scanner;
+
 public class projekSwitch {
     public static void main(String[] args) {
-        
+       Scanner qs = new Scanner(System.in);
+
+       System.out.println("Nilai Matkul");
     }
 }
